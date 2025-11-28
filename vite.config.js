080950a78e2a7aa863d6095b87f95e19356b84a7
lqc <1521028580@qq.com>
2025-11-28 09:58:32 +0800

    Add build files for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/todos_list/',
+  build: {
+    outDir: '.',
+    emptyOutDir: false
+  }
 })
